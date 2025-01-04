1ce726a47c41b84863ca98608af49002b8fe5cc2
Navin Annamalai <62108307+Navin-Annamalai@users.noreply.github.com>
2025-01-04 12:03:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function submitbutton(){
+    var FName = document.getElementById("first1").value;
+    var MName = document.getElementById("middle1").value;
+    var LName = document.getElementById("last1").value;
+    var mail = document.getElementById("email").value;
+    var pass = document.getElementById("pass").value;
+    console.log(FName);
+    console.log(MName);
+    console.log(LName);
+    console.log(mail);
+    console.log(pass);
+}
